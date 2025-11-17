7d106d850558fc8a03587bf935a2918f67fcc030
Gaddala Yacobu <126049931+Yacobujet2022@users.noreply.github.com>
2025-11-17 10:25:15 +0530

    code moved to landing screen

--- WeekOffBT/FlightplanViewController.swift
+++ WeekOffBT/FlightplanViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class FlightplanViewController: UIViewController {
+    
+    lazy var homeTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+        
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        homeTableView.frame = view.bounds
+        self.view.addSubview(homeTableView)
     }
     
 
+}
+
+extension LoginViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        return cell
+    }
 }
--- WeekOffBT/LoginViewController.swift
+++ WeekOffBT/LoginViewController.swift
@@ -9,32 +9,15 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    lazy var homeTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.delegate = self
-        tableView.dataSource = self
-        return tableView
-        
-    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        homeTableView.frame = view.bounds
-        self.view.addSubview(homeTableView)
+
         
     }
 
 
 }
 
-extension LoginViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
-        return cell
-    }
-}
+
